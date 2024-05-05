8e327df18bc68738765dcaff3b4809ffcf346e1d
maxamed1st <maxamed1st@gmail.com>
2024-05-05 20:10:14 +0200

    remove statusbar

--- App.tsx
+++ App.tsx
@@ -1,11 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View tw="flex-1 bg-white items-center justify-center">
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
